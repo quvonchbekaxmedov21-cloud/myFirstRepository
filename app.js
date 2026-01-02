437584f831a2cf1eaebfff3d78c2c898d11d8d98
Clay <quvonchbekaxmedov21@gmail.com>
2026-01-02 16:33:45 +0500

    feat: delete button

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ fs.readFile("database/user.json", "utf8", (err, data) => {
 // MONGODB call
 
 const db = require("./server").db();
+const mongodb = require("mongodb");
 
 // 1: kirish codelari
 app.use(express.static("public"));
@@ -50,12 +51,17 @@ app.post("/create-item", (req, res) => {
     console.log(req.body);
     const new_reja = req.body.reja;
     db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-        if(err) {
-            console.log(err);
-            res.end('something went wrong');
-        } else {
-            res.end("successfully added");
-        }
+        console.log(data.ops);
+       res.json(data.ops[0]);
+    });
+});
+
+app.post("/delete-item", (req, res) => {
+    const id = req.body.id;
+    db.collection("plans").deleteOne(
+        { _id: new mongodb.ObjectId(id)}, 
+        function(err, data) {
+        res.json({state: "success"});
     });
 });
 
--- train.js
+++ train.js
@@ -1,8 +1,88 @@
 /* console.log("train task ishga tushdi"); */
 
+// TASK C
+
+class Shop {
+  constructor(non, lagmon, cola) {
+    this.non = non;
+    this.lagmon = lagmon;
+    this.cola = cola;
+  }
+
+  // Hozirgi vaqtni HH:mm formatida olish uchun yordamchi funksiya
+  getTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    return `${hours}:${minutes}`;
+  }
+
+  // 1. Qoldiq metodi
+  qoldiq() {
+    const time = this.getTime();
+    console.log(
+      `Hozir ${time}'da ${this.non}'ta non, ${this.lagmon}'ta lag'mon va ${this.cola}'ta cola mavjud!`
+    );
+  }
+
+  // 2. Sotish metodi
+  sotish(mahsulot, miqdor) {
+    if (this[mahsulot] !== undefined) {
+      if (this[mahsulot] >= miqdor) {
+        this[mahsulot] -= miqdor;
+      } else {
+        console.log(`Kechirasiz, omborda yetarli ${mahsulot} yo'q.`);
+      }
+    } else {
+      console.log("Bunday mahsulot do'konimizda mavjud emas.");
+    }
+  }
+
+  // 3. Qabul metodi
+  qabul(mahsulot, miqdor) {
+    if (this[mahsulot] !== undefined) {
+      this[mahsulot] += miqdor;
+    } else {
+      console.log("Biz faqat non, lagmon va cola qabul qilamiz.");
+    }
+  }
+}
+
+// --- Tekshirish ---
+const shop = new Shop(4, 5, 2);
+
+shop.qoldiq(); // Dastlabki holat
+
+shop.sotish("non", 3); 
+shop.qabul("cola", 4); 
+
+shop.qoldiq(); // O'zgargan holat
+
+shop.sotish("non", 6);
+shop.qabul("cola", 8);
+shop.qabul("fanta", 4);
+
+shop.qoldiq();
+
+shop.sotish("non", 6);
+shop.qabul("cola", 8);
+shop.qabul("non", 8);
+shop.qabul("fanta", 4);
+
+shop.qoldiq();
+
+shop.sotish("non", 6);
+shop.sotish("cola", 8);
+shop.qabul("non", 8);
+shop.qabul("fanta", 4);
+
+shop.qoldiq();
+
+
+
 // TASK B
 
-function countDigits(number) {
+/* function countDigits(number) {
 let count = 0;
 for (let i =0; i < number.length; i++) {
     if (number[i] >='0' && number[i] <='9') {
@@ -19,7 +99,9 @@ const result1 = countDigits("ad2a44rr554y798yy78867et0sfgb9")
 console.log(result1);
 
 const result3 = countDigits("hfghfgh68jhjh77gjh686hdhddh93939nf8f8f9fn33nf9fn")
-console.log(result3);
+console.log(result3); */
+
+
 //TASK A
 /* function countLetter(letter, word) {
     let count = 0;
